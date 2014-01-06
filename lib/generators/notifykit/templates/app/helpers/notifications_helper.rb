9218d94fa62383c204762d4dbb75538ef245412a
Jeff Rafter <jeffrafter@gmail.com>
2014-01-06 13:54:40 -0800

    Helper specs

--- lib/generators/notifykit/templates/app/helpers/notifications_helper.rb
+++ lib/generators/notifykit/templates/app/helpers/notifications_helper.rb
@@ -1,10 +1,12 @@
 module NotificationsHelper
   def notification_company_name
     # TODO According to CANSPAM you must include company contact information
+    <% if options.test_mode? %>"Test Company"<% end %>
   end
 
   def notification_company_address
     # TODO According to CANSPAM you must include company contact information
+    <% if options.test_mode? %>"Test Company Address"<% end %>
   end
 
   def recent_notifications
--- lib/generators/notifykit/templates/spec/helpers/notifications_helper_spec.rb
+++ lib/generators/notifykit/templates/spec/helpers/notifications_helper_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the NotificationsHelper. For example:
-#
-# describe NotificationsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe NotificationsHelper do
-  it "returns the company name for use in notifications"
-  it "returns the company address for use in notifications"
-  it "finds recent notifications"
+  let(:user) { User.create(email: "test@example.com"); User.first }
+
+  it "returns the company name for use in notifications" do
+    # If this is failing it is because you need to add your company name
+    # to app/helpers/notifications_helper.rb
+    helper.notification_company_name.should_not be_blank
+  end
+
+  it "returns the company address for use in notifications" do
+    # If this is failing it is because you need to add your company address
+    # to app/helpers/notifications_helper.rb
+    helper.notification_company_name.should_not be_blank
+  end
+
+  it "finds recent notifications" do
+    helper.stub(:current_user).and_return(user)
+    4.times { create(:notification, user: user) }
+    helper.recent_notifications.count.should == 3
+  end
 end
